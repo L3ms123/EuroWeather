9dca0d86a914d46a5633ac630f3303772c01c91f
queralts <quersalv@gmail.com>
2025-12-23 22:52:11 +0100

    Fix merge

--- scripts/load_grids.py
+++ scripts/load_grids.py
@@ -6,17 +6,8 @@ import math
 # CONFIGURATION
 # ----------------------------------
 
-<<<<<<< HEAD
 CITY_JSON = "data/ciudades_eu_km2.json"
 OUTPUT_JSON = "data/ciudades_eu_km2_with_grids.json"
-=======
-TTL_DAYS = 7
-TTL_SECONDS = TTL_DAYS * 24 * 3600
-
-DYNAMODB_TABLE = "WeatherReadings"
-CITY_JSON = "data/ciudades_eu_km2.json"
-session = requests.Session()
->>>>>>> 373c7ad2989a55ca884800bb44e2d53a76a4b787
 
 # ----------------------------------
 # UTILS
@@ -53,91 +44,6 @@ def generate_city_grids(center_lat, center_lon, area_km2, precision=5):
 
     return grids
 
-<<<<<<< HEAD
-=======
-# Obtain index of the time closest to the current time
-def get_current_hour_index(times):
-    now = datetime.now(timezone.utc)
-    best_idx = 0
-    min_diff = float("inf")
-
-    for i, t in enumerate(times):
-        t_dt = datetime.fromisoformat(t).replace(tzinfo=timezone.utc)
-        diff = abs((t_dt - now).total_seconds())
-        if diff < min_diff:
-            min_diff = diff
-            best_idx = i
-
-    return best_idx
-
-# Obtain time data from Open-Meteo for a given grid
-def fetch_hourly(lat, lon, retries=5):
-    url = "https://api.open-meteo.com/v1/forecast"
-    params = {
-        "latitude": lat,
-        "longitude": lon,
-        "hourly": ["temperature_2m", "relativehumidity_2m", "windspeed_10m", "precipitation", "precipitation_probability"],
-        "timezone": "UTC"
-    }
-
-    for attempt in range(retries):
-        try:
-            r = session.get(url, params=params, timeout=10)
-            r.raise_for_status()
-            return r.json()
-        except requests.exceptions.RequestException as e:
-            wait = 2 ** attempt + random.random()
-            print(f"Open-Meteo error, retrying in {wait:.1f}s...")
-            time.sleep(wait)
-
-    raise RuntimeError("Open-Meteo failed after retries")
-
-
-# Store data on DynamoDB
-def store_grid_data(weather_table, city, lat, lon, geohash):
-    data = fetch_hourly(lat, lon)
-
-    times = data["hourly"]["time"]
-    temps = data["hourly"]["temperature_2m"]
-    hums  = data["hourly"]["relativehumidity_2m"]
-    winds = data["hourly"]["windspeed_10m"]
-    rains = data["hourly"]["precipitation"]
-    rain_prob = data["hourly"]["precipitation_probability"]
-
-    idx = get_current_hour_index(times)
-
-    ts_iso = times[idx] + "Z"
-    ts_unix = int(
-        datetime.fromisoformat(times[idx])
-        .replace(tzinfo=timezone.utc)
-        .timestamp()
-    )
-
-    # ensure no null values
-    rain_value = rains[idx] if rains[idx] is not None else 0
-    rain_prob_value = rain_prob[idx] if rain_prob[idx] is not None else 0
-
-    item = {
-        "PK": f"grid#{geohash}",
-        "SK": f"ts#{ts_iso}",
-        "lat": Decimal(str(lat)),
-        "lon": Decimal(str(lon)),
-        "geohash": geohash,
-        "city_name": city,
-        "temp": Decimal(str(temps[idx])),
-        "humidity": Decimal(str(hums[idx])),
-        "wind_speed": Decimal(str(winds[idx])),
-        "precipitation": Decimal(str(rain_value)),
-        "precipitation_prob": Decimal(str(rain_prob_value)),  
-        "timestamp": ts_unix,
-        "ttl": ts_unix + TTL_SECONDS
-    }
-
-    weather_table.put_item(Item=item)
-
-    print(f"Inserted current hour for {city} - {geohash}")
-
->>>>>>> 373c7ad2989a55ca884800bb44e2d53a76a4b787
 # ----------------------------------
 # MAIN
 # ----------------------------------
